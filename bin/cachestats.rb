2f25b685182764add06e9a4845991e736eaa8fb0
Reda NOUSHI <reda_noushi@yahoo.com>
2013-11-19 08:38:47 +0000

    Report retcode if envvar DEBUG is defined, and exit with non-zero retcode.

--- bin/cachestats.rb
+++ bin/cachestats.rb
@@ -7,6 +7,9 @@ filename=ARGV[0]
 
 retcode, pieces = File.mincore(filename)
 
+puts "retcode=#{retcode}" if ENV["DEBUG"]
+exit retcode unless retcode == 0
+
 cached=0
 pieces.each do |e|
   cached += 1 if e
